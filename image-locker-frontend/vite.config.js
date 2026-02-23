6dcab94d0eb457832b8180e8b0258cfd2a80298d
devops4 <sravankumar.v7891@gmail.com>
2026-02-23 14:33:29 -0500

    Added dockerfiles and docker compose file

--- image-locker-frontend/vite.config.js
+++ image-locker-frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+const backend = process.env.DOCKER
+  ? "http://backend:8080"
+  : "http://localhost:8080";
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: backend,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
